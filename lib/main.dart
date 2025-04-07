f4ab1536f84ede122c89606f21d68cc1efe083e6
yebintt <zsxcfv@hanyang.ac.kr>
2025-04-07 12:24:58 +0900

    EOS 폴더 제거 및 내부 파일 루트로 이동

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:eos_advance_login/firebase_options.dart';
+import 'package:eos_advance_login/screens/home_screen.dart';
+import 'package:eos_advance_login/service/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:eos_advance_login/screens/login_screen.dart';
+import 'package:eos_advance_login/theme/light_theme.dart';
+import 'package:eos_advance_login/theme/foundation/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: [과제 2-1] 카카오 SDK 초기화
+  /*
+   * 카카오 SDK 초기화 코드
+   * - 카카오 개발자 콘솔에서 발급받은 네이티브 앱 키를 사용하여 초기화
+   * - KakaoSdk.init(nativeAppKey: '네이티브_앱_키') 호출
+   */
+
+  KakaoSdk.init(
+      nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'] ?? '',
+      javaScriptAppKey: dotenv.env['KAKAO_JS_APP_KEY'] ?? '');
+
+  // Firebase 초기화 수정
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform, // 필요시 주석 해제
+    );
+    print('Firebase 초기화 성공');
+  } catch (e) {
+    print('Firebase 초기화 오류: $e');
+  }
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+/// 애플리케이션의 루트 위젯
+/// - 앱의 전체 테마 및 초기 화면을 설정합니다.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = LightTheme();
+
+    return MaterialApp(
+      title: 'EOS Advance Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: theme.color.primary),
+        useMaterial3: true,
+        fontFamily: 'Pretendard', // 프리텐다드 폰트 기본 적용
+      ),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // 인증 상태 확인 중일 때 로딩 표시
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          // 로그인된 유저가 있으면 HomeScreen, 없으면 LoginScreen
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          } else {
+            return const LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
